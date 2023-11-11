5b0d452ece897e26c142e7ad1467a92557853fd2
amar <amarmudiraj@gmail.com>
2023-11-11 11:48:30 +0530

    MY TWO NEWEST Bucket

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "google_storage_bucket" "bucket-004" {
 resource "google_storage_bucket" "bucket-002" {
   name     = "-devops-bucket-2023-nov-2001--001"
   location = "US"
-#3
+  #3
 }
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "bucket_name" {
   description = "Name of Bucket"
-  default     = "dev-bucket-2023-nov-2001--007"
-
+  default     = "dev-bucket-2023-nov-2001--100"
 }
