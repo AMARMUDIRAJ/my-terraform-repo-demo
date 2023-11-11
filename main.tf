22856a21afc7b3ced0279fdb002930a367f74581
amar <amarmudiraj@gmail.com>
2023-11-11 11:35:38 +0530

    udpated the Access Control List of Bucket

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 resource "google_storage_bucket" "bucket-001" {
   name     = var.bucket_name
   location = "US"
+
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
 }
